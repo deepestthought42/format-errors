ae2f34c361731c48f64811d15642e03077dec914
Renee Klawitter <deepestthought42@gmail.com>
2015-09-21 17:58:41 -0700

    detect digits

--- format-errors.lisp
+++ format-errors.lisp
@@ -4,28 +4,49 @@
 
 ;;; "format-errors" goes here. Hacks and glory await!
 
+(defparameter *base* 10)
+(defparameter *separator* '-)
 
 
 (defclass number-details ()
   ((value :accessor value :initarg :value
 	  :initform (error "Need to supply value."))
-   (digits<1 :accessor digits<1 :initarg :digits<1 :initform '())
-   (digits>1 :accessor digits>1 :initarg :digits>1 :initform '())
+   (digits :accessor digits :initarg :digits :initform '())
+   (digits-down-to :accessor digits-down-to :initarg :digits-down-to :initform 0)
    (order-of-magnitude :accessor order-of-magnitude
 		       :initarg :order-of-magnitude :initform 1d0)
    (sign :accessor sign :initarg :sign :initform 1d0)))
 
 
-(defun %get-digits>1 (value order-of-magnitude)
-  (iter
-    (for i from 0 to order-of-magnitude)
-    (collect (round (/ value (expt 10 i))))))
+(defun %get-order-of-magnitude (value)
+  (round (log value *base*)))
+
+(defun %get-sign (value)
+  (/ value (abs value)))
+
+(defun %get-digits (value down-to)
+  (let ((order-of-magnitude (%get-order-of-magnitude value)))
+    (if (< order-of-magnitude 0)
+	'()
+	(iter
+	  ;; rounding should take care of weird floating point effects
+	  (with new-value = (* (round value (expt *base* down-to))
+			       (expt *base* down-to)))
+	  (for i from order-of-magnitude downto down-to)
+	  (for digit = (floor (/ new-value (expt *base* i))))
+	  (decf new-value (* digit (expt *base* i)))
+	  (if (= -1 i)
+	      (progn (collect *separator*) (collect digit))
+	      (collect digit))))))
+
+
+
 
 (defmethod initialize-instance :after ((obj number-details) &key)
   (with-slots (order-of-magnitude sign value)
       obj
-    (setf order-of-magnitude (round (log value 10d0))
-	  sign (/ value (abs value)))))
+    (setf order-of-magnitude (%get-order-of-magnitude value)
+	  sign (%get-sign value))))
 
 
 
